d72e167286a829dbcea1e87123585fed1b356a01
Fma965 <admin@cyanlabs.net>
2019-03-31 22:36:21 +0100

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,9 +50,9 @@
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(294, 29);
+            this.comboBox1.Location = new System.Drawing.Point(16, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(39, 21);
+            this.comboBox1.Size = new System.Drawing.Size(52, 21);
             this.comboBox1.TabIndex = 3;
             // 
             // lblOSD
@@ -136,11 +136,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(10, 15);
+            this.button1.Location = new System.Drawing.Point(3, 1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(13, 23);
             this.button1.TabIndex = 32;
-            this.button1.Text = "button1";
+            this.button1.Text = "S";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
